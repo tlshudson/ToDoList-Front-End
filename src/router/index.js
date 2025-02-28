7a31af62494537a1f498ea6e0bfb5f89a8ebce43
tlshudson <hudsonteles00@gmail.com>
2025-02-27 23:52:11 -0300

    add component Login in Vue Router

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Login from 'src/views/Login.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    }
   ],
 })
 
