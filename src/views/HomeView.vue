ce02a6577cdac9213be9aab80b571bb85671df03
Hudson <hudsonteles00@gmail.com>
2025-03-24 08:07:20 -0300

    Atualização componenente Home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,14 @@
 import TheWelcomme from '.../components/TheWelcome.vue'
 <template>
+  <div class="home"></div>
   <main>
     <TheWelcome/>
   </main>
 </template>
+
+<script>
+  export default {
+    name: 'home',
+    components: {},
+  }
+</script>
